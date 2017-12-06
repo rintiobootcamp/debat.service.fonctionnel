06ac6c754957328121f3d37b692ac75f681cd9b8
ibrahim00017 <hibrahimyya@gmail.com>
2017-12-06 18:52:15 +0100

    implementation of debat sf

---
+++ src/main/java/com/bootcamp/controllers/DebatController.java
@@ -0,0 +1,57 @@
+package com.bootcamp.controllers;
+
+import com.bootcamp.commons.exceptions.DatabaseException;
+import com.bootcamp.entities.Debat;
+import com.bootcamp.services.DebatService;
+import com.bootcamp.version.ApiVersions;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController("DebatContoller")
+@RequestMapping("/debats")
+@Api(value = "Debat API", description = "Debat API")
+@CrossOrigin(origins = "*")
+public class DebatController {
+//
+
+    @Autowired
+    DebatService debatService;
+
+    @Autowired
+    HttpServletRequest request;
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ApiVersions({"1.0"})
+    @ApiOperation(value = "Create a debat", notes = "Create a debat")
+    public ResponseEntity<Debat> create(@RequestBody @Valid Debat debat) throws SQLException {
+        debat = debatService.create(debat);
+        return new ResponseEntity<>(debat, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ApiVersions({"1.0"})
+    @ApiOperation(value = "Read All Debats", notes = "Read aall the Debats")
+    public ResponseEntity<List<Debat>> read() throws SQLException, IllegalAccessException, DatabaseException, InvocationTargetException {
+        List<Debat> debats = debatService.read(request);
+        return new ResponseEntity<List<Debat>>(debats, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ApiVersions({"1.0"})
+    @ApiOperation(value = "Get one Debats", notes = "Read a particular Debats")
+    public ResponseEntity<Debat> getById(@PathVariable int id) throws SQLException, IllegalAccessException, DatabaseException, InvocationTargetException {
+        Debat debat = debatService.read(id);
+        return new ResponseEntity<Debat>(debat, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/bootcamp/services/DebatService.java
@@ -0,0 +1,74 @@
+package com.bootcamp.services;
+
+
+import com.bootcamp.commons.constants.DatabaseConstants;
+import com.bootcamp.commons.exceptions.DatabaseException;
+import com.bootcamp.commons.models.Criteria;
+import com.bootcamp.commons.models.Criterias;
+import com.bootcamp.commons.ws.utils.RequestParser;
+import com.bootcamp.crud.DebatCRUD;
+import com.bootcamp.entities.Debat;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.List;
+
+@Component
+public class DebatService implements DatabaseConstants {
+
+    DebatCRUD debatCRUD;
+
+
+    @PostConstruct
+    public void init(){
+        debatCRUD = new DebatCRUD();
+    }
+
+    public Debat create(Debat debat) throws SQLException {
+        debat.setDateMiseAJour(System.currentTimeMillis());
+        debatCRUD.create(debat);
+        return debat;
+    }
+
+    public void update(Debat debat) throws SQLException {
+        debatCRUD.update(debat);
+    }
+
+    public Debat delete(int id) throws SQLException {
+        Debat debat = read(id);
+        debatCRUD.delete(debat);
+
+        return debat;
+    }
+
+    public Debat read(int id) throws SQLException {
+        Criterias criterias = new Criterias();
+        criterias.addCriteria(new Criteria("id", "=", id));
+        List<Debat> debats = debatCRUD.read(criterias);
+
+        return debats.get(0);
+    }
+
+
+
+    public List<Debat> read(HttpServletRequest request) throws SQLException, IllegalAccessException, DatabaseException, InvocationTargetException {
+        Criterias criterias = RequestParser.getCriterias(request);
+        List<String> fields = RequestParser.getFields(request);
+        List<Debat> debats = null;
+        if(criterias == null && fields == null)
+            debats =  debatCRUD.read();
+        else if(criterias!= null && fields==null)
+            debats = debatCRUD.read(criterias);
+        else if(criterias== null && fields!=null)
+            debats = debatCRUD.read(fields);
+        else
+            debats = debatCRUD.read(criterias, fields);
+
+        return debats;
+    }
+
+}
+
