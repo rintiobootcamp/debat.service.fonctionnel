ae2896c26e6aaa7e3d3bfb6394d3afa9178d799f
ibrahim00017 <hibrahimyya@gmail.com>
2018-07-24 17:52:30 +0100

    elast

--- src/main/java/com/bootcamp/services/DebatService.java
+++ src/main/java/com/bootcamp/services/DebatService.java
@@ -14,6 +14,7 @@ import com.rintio.elastic.client.ElasticClient;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
@@ -33,7 +34,8 @@ import javax.persistence.TypedQuery;
 @Component
 public class DebatService implements DatabaseConstants {
 ElasticClient elasticClient;
-public DebatService(){
+    @PostConstruct
+public void DebatService(){
     elasticClient = new ElasticClient();
 }
 
